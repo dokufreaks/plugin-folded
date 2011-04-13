2eba3c2f6cf26ad38f6852b63b0601290cda9546
Matthias Schulte <post@lupo49.de>
2011-04-13 20:24:48 +0200

    translations: added gl (galician), no (norwegian), pt-br (portugese), th (thai)

---
+++ lang/no/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * norwegian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Matthias Schulte <mailinglist@lupo49.de>
+ */
+
+// custom language strings for the plugin
+$lang['reveal']     = 'vis';
+$lang['reveallong'] = 'vis skjult innhold ';
+
+$lang['hide']       = 'skjul';
+$lang['hidelong']   = 'skjul innhold';
+
+//Setup VIM: ex: et ts=2 :
