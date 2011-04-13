fc21b236da11dc7ff687bb739d14bca69a32f201
Matthias Schulte <post@lupo49.de>
2011-04-13 20:42:22 +0200

    de: language file for admin settings

---
+++ lang/de/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * German language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Matthias Schulte <mailinglist@lupo49.de>
+ */
+ 
+// for the configuration manager
+$lang['hide']  = 'Tooltip zum Zuklappen<br />(Feld leer lassen um den Standardwert zu nutzen)';
+$lang['reveal']  = 'Tooltip zum Aufklappen<br />(Feld leer lassen um den Standardwert zu nutzen)';
