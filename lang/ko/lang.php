614b18899127f2226a0414571ea7001a81e0ec5d
Myeongjin <aranet100@gmail.com>
2013-08-13 18:35:05 +0200

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -1,16 +1,12 @@
 <?php
+
 /**
- * korean language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Esther Brunner <esther@kaffeehaus.ch>
+ * @author Myeongjin <aranet100@gmail.com>
  */
-
-// custom language strings for the plugin
-$lang['reveal']     = '드러냄';
-$lang['reveallong'] = '숨겨져있는 내용을 드러냅니다';
-
-$lang['hide']       = '숨김';
-$lang['hidelong']   = '내용을 숨깁니다';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['reveal']                = '들추기';
+$lang['reveallong']            = '숨겨진 내용을 들춥니다';
+$lang['hide']                  = '숨기기';
+$lang['hidelong']              = '내용을 숨깁니다';
