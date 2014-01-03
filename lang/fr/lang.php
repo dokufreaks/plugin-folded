eada9e48133bea1f8d32cabf528245c12d146950
Fabrice Dejaigher <fabrice@chtiland.com>
2014-01-03 12:25:06 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,16 +1,12 @@
 <?php
+
 /**
- * french language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Esther Brunner <esther@kaffeehaus.ch>
+ * 
+ * @author Esther Brunner <esther@kaffeehaus.ch>
+ * @author Fabrice Dejaigher <fabrice@chtiland.com>
  */
-
-// custom language strings for the plugin
-$lang['reveal']     = 'révéler';
-$lang['reveallong'] = 'révéler le contenu caché';
-
-$lang['hide']       = 'cacher';
-$lang['hidelong']   = 'cacher le contenu';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['reveal']                = 'révéler';
+$lang['reveallong']            = 'révéler le contenu masqué';
+$lang['hide']                  = 'masquer';
+$lang['hidelong']              = 'masquer le contenu';
