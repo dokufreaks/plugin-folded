245febf63c592cb701c7e2cfb9a95c88e87c57b0
Robert Bogenschneider <bogi@uea.org>
2014-02-23 08:30:05 +0100

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['hide']                  = 'Helpindiko por kaŝi<br />(lasu malplena por uzi tradukon)';
+$lang['reveal']                = 'Helpindiko por malkaŝi<br />(lasu malplena por uzi tradukon)';
