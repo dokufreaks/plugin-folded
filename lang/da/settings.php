933b854394757ea57eb6e8eb9b38880ad7cc59b8
Søren Birk <soer9648@eucl.dk>
2014-12-10 10:40:05 +0100

    translation update

---
+++ lang/da/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Søren Birk <soer9648@eucl.dk>
+ */
+$lang['hide']                  = 'Foldet tooltip som skjules<br />(efterlad blankt for at benytte lokaliseret streng)';
+$lang['reveal']                = 'Foldet tooltip som vises<br />(efterlad blankt for at benytte lokaliseret streng)';
