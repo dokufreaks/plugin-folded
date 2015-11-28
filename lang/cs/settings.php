30aa743fd1facf5db16b53bcb07beaeaafcd936e
Jaroslav Lichtblau <jlichtblau@seznam.cz>
2015-11-28 14:20:06 +0100

    translation update

---
+++ lang/cs/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
+ */
+$lang['hide']                  = 'Zasunout skrývací nabídku<br />(nechat prázdné pro použití přeložených textů)';
+$lang['reveal']                = 'Vysunout skrývací nabídku<br />(nechat prázdné pro použití přeložených textů)';
