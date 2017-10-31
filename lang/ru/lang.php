88323fbfe7eeb5124c94a6faf90f61086267c48d
Aleksandr Selivanov <alexgearbox@yandex.ru>
2017-10-31 21:00:13 +0100

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -2,10 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author     Esther Brunner <esther@kaffeehaus.ch>
+ *
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ * @author Esther Brunner <esther@kaffeehaus.ch>
  */
-$lang['reveal']                = 'открыть';
-$lang['reveallong']            = 'показать скрытое оглавление';
+$lang['reveal']                = 'раскрыть';
+$lang['reveallong']            = 'показать скрытое содержимое';
 $lang['hide']                  = 'скрыть';
-$lang['hidelong']              = 'скрыть оглавление';
+$lang['hidelong']              = 'скрыть содержимое';
+$lang['fold_unfold_all_button'] = 'Свернуть / развернуть всё';
