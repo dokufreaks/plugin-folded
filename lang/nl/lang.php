b8d2ab08a4c7e5de79f46906184096d49cc7810a
Gerrit Uitslag <klapinklapin@gmail.com>
2019-08-12 20:55:26 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -2,10 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author     Esther Brunner <esther@kaffeehaus.ch>
+ *
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ * @author Esther Brunner <esther@kaffeehaus.ch>
  */
 $lang['reveal']                = 'tonen';
 $lang['reveallong']            = 'toon verborgen inhoud';
 $lang['hide']                  = 'verbergen';
 $lang['hidelong']              = 'verberg inhoud';
+$lang['fold_unfold_all_button'] = 'Alles vouwen/uitvouwen';
