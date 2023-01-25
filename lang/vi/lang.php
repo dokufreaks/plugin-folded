5910bc31df478416cac199124784012870ce2f6e
Minh <phandinhminh@protonmail.ch>
2023-01-25 15:30:24 +0100

    translation update

---
+++ lang/vi/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['reveal']                = 'tiết lộ';
+$lang['reveallong']            = 'tiết lộ nội dung ẩn';
+$lang['hide']                  = 'ẩn';
+$lang['hidelong']              = 'ẩn nội dung';
+$lang['fold_unfold_all_button'] = 'Gấp/mở gấp tất cả';
