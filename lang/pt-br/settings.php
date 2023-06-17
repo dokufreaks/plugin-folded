58e6be1860dac060d4a61c1c7c4661cac5078038
Daniel Dias Rodrigues <danieldiasr@gmail.com>
2023-06-17 18:50:33 +0200

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
+ */
+$lang['hide']                  = 'Mensagem (dica) para ocultar<br />(deixe em branco para usar a string localizada)';
+$lang['reveal']                = 'Mensagem (dica) para mostrar<br />(deixe em branco para usar a string localizada)';
+$lang['unfold_default']        = 'Mostre tudo por padrão';
+$lang['show_fold_unfold_all_button'] = 'Mostrar um botão na área de ferramenta de página que mostra/oculta todos os textos da página atual';
