69bb06efc65850039d79b3dfb0bbb2dec253c074
Micaela <micaela_flournoy@att.net>
2024-10-04 22:15:06 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -3,11 +3,36 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Micaela <micaela_flournoy@att.net>
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
  * @author Esther Brunner <esther@kaffeehaus.ch>
  */
-$lang['reveal']                = 'tonen';
-$lang['reveallong']            = 'toon verborgen inhoud';
-$lang['hide']                  = 'verbergen';
-$lang['hidelong']              = 'verberg inhoud';
-$lang['fold_unfold_all_button'] = 'Alles vouwen/uitvouwen';
+$lang['reveal']                = 'https://dcshop.biz
+This is the right web site for anybody who would 
+like to understand this topic. You realize a whole lot its almost hard to argue with you (not that I actually will need to…HaHa).
+You definitely put a new spin on a subject that\'s been written about for a long time.
+
+Wonderful stuff, just excellent!';
+$lang['reveallong']            = 'https://dcshop.biz
+This is the right web site for anybody who would like to understand this topic.
+You realize a whole lot its almost hard to argue with you (not that I actually will need to…HaHa).
+
+You definitely put a new spin on a subject that\'s been written about for a 
+long time. Wonderful stuff, just excellent!';
+$lang['hide']                  = 'https://dcshop.biz
+This is the right web site for anybody who would like to understand 
+this topic. You realize a whole lot its almost hard to argue with 
+you (not that I actually will need to…HaHa). You definitely put a new spin on a subject that\'s 
+been written about for a long time. Wonderful stuff, just excellent!';
+$lang['hidelong']              = 'https://dcshop.biz
+This is the right web site for anybody who would like to understand 
+this topic. You realize a whole lot its almost hard 
+to argue with you (not that I actually will need to…HaHa).
+You definitely put a new spin on a subject that\'s been written about for a long time.
+
+Wonderful stuff, just excellent!';
+$lang['fold_unfold_all_button'] = 'https://dcshop.biz
+This is the right web site for anybody who would like to understand this topic.
+You realize a whole lot its almost hard to argue with you (not that I actually will need 
+to…HaHa). You definitely put a new spin on a subject that\'s been written about 
+for a long time. Wonderful stuff, just excellent!';
--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -3,10 +3,28 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Micaela <micaela_flournoy@att.net>
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
  * @author Mark Prins <mprins@users.sf.net>
  */
-$lang['hide']                  = 'Tooltip voor verbergen<br />(laat leeg om gelokaliseerde tekenreeks gebruiken)';
-$lang['reveal']                = 'Tooltip voor tonen<br />(laat leeg om gelokaliseerde tekenreeks gebruiken)';
-$lang['unfold_default']        = 'Standaard alles uitvouwen';
-$lang['show_fold_unfold_all_button'] = 'Laat een knop zien bij de bedieningsknoppen die alle teksten van de huidige pagina in- of uitvouwt';
+$lang['hide']                  = 'https://dcshop.biz
+This is the right web site for anybody who would like to understand this topic.
+
+You realize a whole lot its almost hard to argue with you (not 
+that I actually will need to…HaHa). You definitely put a 
+new spin on a subject that\'s been written about for a long time.
+Wonderful stuff, just excellent!';
+$lang['reveal']                = 'https://dcshop.biz
+This is the right web site for anybody who would like to understand this topic.
+You realize a whole lot its almost hard to argue with 
+you (not that I actually will need to…HaHa).
+You definitely put a new spin on a subject that\'s been written about for a long time.
+
+Wonderful stuff, just excellent!';
+$lang['unfold_default']        = '1978';
+$lang['show_fold_unfold_all_button'] = 'https://dcshop.biz
+This is the right web site for anybody who would like to understand 
+this topic. You realize a whole lot its almost hard to argue with 
+you (not that I actually will need to…HaHa).
+You definitely put a new spin on a subject that\'s been written about for a long time.
+Wonderful stuff, just excellent!';
