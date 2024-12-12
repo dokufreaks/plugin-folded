261c415247e530175f431fce8d01c0d4ef2e9b4f
Ola <olaladner@aol.com>
2024-12-12 09:20:46 +0100

    translation update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -5,11 +5,28 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Ola <olaladner@aol.com>
  * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
  * @author Matthias Schulte <mailinglist@lupo49.de>
  */
-$lang['reveal']                = 'mostrar';
-$lang['reveallong']            = 'mostrar conteúdo oculto';
-$lang['hide']                  = 'ocultar';
-$lang['hidelong']              = 'ocultar conteúdo';
-$lang['fold_unfold_all_button'] = 'Ocultar/mostrar tudo';
+$lang['reveal']                = 'https://Mcpipokinha23.shop
+
+Every weekend i used to go to see this website, for the reason that i wish for enjoyment, since this 
+this web page conations actually good funny data too.';
+$lang['reveallong']            = 'https://Mcpipokinha23.shop
+
+Every weekend i used to go to see this website, for the reason that i wish for enjoyment, since this this web page conations actually good funny data too.';
+$lang['hide']                  = 'https://Mcpipokinha23.shop
+
+Every weekend i used to go to see this website, for the 
+reason that i wish for enjoyment, since this this web 
+page conations actually good funny data too.';
+$lang['hidelong']              = 'https://Mcpipokinha23.shop
+
+Every weekend i used to go to see this website, for the 
+reason that i wish for enjoyment, since this this web page conations actually good 
+funny data too.';
+$lang['fold_unfold_all_button'] = 'https://Mcpipokinha23.shop
+
+Every weekend i used to go to see this website, for the 
+reason that i wish for enjoyment, since this this web page conations actually good funny data too.';
--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -3,9 +3,21 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Ola <olaladner@aol.com>
  * @author Daniel Dias Rodrigues <danieldiasr@gmail.com>
  */
-$lang['hide']                  = 'Mensagem (dica) para ocultar<br />(deixe em branco para usar a string localizada)';
-$lang['reveal']                = 'Mensagem (dica) para mostrar<br />(deixe em branco para usar a string localizada)';
-$lang['unfold_default']        = 'Mostre tudo por padrão';
-$lang['show_fold_unfold_all_button'] = 'Mostrar um botão na área de ferramenta de página que mostra/oculta todos os textos da página atual';
+$lang['hide']                  = 'https://Mcpipokinha23.shop
+
+Every weekend i used to go to see this website, for the reason that i wish for enjoyment,
+since this this web page conations actually good funny data too.';
+$lang['reveal']                = 'https://Mcpipokinha23.shop
+
+Every weekend i used to go to see this website, for the 
+reason that i wish for enjoyment, since this this web page 
+conations actually good funny data too.';
+$lang['unfold_default']        = '1976';
+$lang['show_fold_unfold_all_button'] = 'https://Mcpipokinha23.shop
+
+Every weekend i used to go to see this website, for the reason that i 
+wish for enjoyment, since this this web page conations 
+actually good funny data too.';
