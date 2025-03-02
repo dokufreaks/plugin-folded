968b5a6f0b1e213529397eccb3717724a0f7411d
Hai <haishapcott@live.nl>
2025-03-02 16:50:06 +0100

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -3,11 +3,40 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Hai <haishapcott@live.nl>
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
  * @author Esther Brunner <esther@kaffeehaus.ch>
  */
-$lang['reveal']                = 'tonen';
-$lang['reveallong']            = 'toon verborgen inhoud';
-$lang['hide']                  = 'verbergen';
-$lang['hidelong']              = 'verberg inhoud';
-$lang['fold_unfold_all_button'] = 'Alles vouwen/uitvouwen';
+$lang['reveal']                = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/ 
+Right here is the perfect webpage for anyone who 
+wants to find out about this topic. You realize a whole lot 
+its almost hard to argue with you (not that I 
+personally would want to…HaHa). You definitely put 
+a brand new spin on a subject which has been written about 
+for decades. Wonderful stuff, just wonderful!
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/';
+$lang['reveallong']            = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/ 
+Right here is the perfect webpage for anyone who wants to find out about this topic.
+You realize a whole lot its almost hard to argue with 
+you (not that I personally would want to…HaHa).
+You definitely put a brand new spin on a subject which has been written about for decades.
+Wonderful stuff, just wonderful! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/';
+$lang['hide']                  = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/ 
+Right here is the perfect webpage for anyone who wants to find 
+out about this topic. You realize a whole lot its almost hard to argue with you (not 
+that I personally would want to…HaHa). You definitely put 
+a brand new spin on a subject which has been written about for decades.
+Wonderful stuff, just wonderful! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/';
+$lang['hidelong']              = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/ 
+Right here is the perfect webpage for anyone who wants to find 
+out about this topic. You realize a whole lot its almost hard to argue with you (not that I personally would 
+want to…HaHa). You definitely put a brand new spin on a subject 
+which has been written about for decades. Wonderful stuff, just wonderful!
+
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/';
+$lang['fold_unfold_all_button'] = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/ 
+Right here is the perfect webpage for anyone who wants 
+to find out about this topic. You realize a whole lot its 
+almost hard to argue with you (not that I personally would want to…HaHa).
+You definitely put a brand new spin on a subject which has been written about for decades.
+Wonderful stuff, just wonderful! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/';
--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -3,10 +3,27 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Hai <haishapcott@live.nl>
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
  * @author Mark Prins <mprins@users.sf.net>
  */
-$lang['hide']                  = 'Tooltip voor verbergen<br />(laat leeg om gelokaliseerde tekenreeks gebruiken)';
-$lang['reveal']                = 'Tooltip voor tonen<br />(laat leeg om gelokaliseerde tekenreeks gebruiken)';
-$lang['unfold_default']        = 'Standaard alles uitvouwen';
-$lang['show_fold_unfold_all_button'] = 'Laat een knop zien bij de bedieningsknoppen die alle teksten van de huidige pagina in- of uitvouwt';
+$lang['hide']                  = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/ 
+Right here is the perfect webpage for anyone who wants to find out about this topic.
+
+You realize a whole lot its almost hard to argue with you 
+(not that I personally would want to…HaHa). You definitely 
+put a brand new spin on a subject which has been written about for decades.
+Wonderful stuff, just wonderful! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/';
+$lang['reveal']                = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/ 
+Right here is the perfect webpage for anyone who wants 
+to find out about this topic. You realize a whole lot its almost 
+hard to argue with you (not that I personally 
+would want to…HaHa). You definitely put a brand new spin on a subject 
+which has been written about for decades. Wonderful stuff, just wonderful!
+https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/';
+$lang['unfold_default']        = '1987';
+$lang['show_fold_unfold_all_button'] = 'https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/ 
+Right here is the perfect webpage for anyone who wants to 
+find out about this topic. You realize a whole lot its almost hard to argue with you (not that I personally would want to…HaHa).
+You definitely put a brand new spin on a subject which has been written about for decades.
+Wonderful stuff, just wonderful! https://xaydungtrangtrinoithat.com/nha-thau-xay-dung-tai-binh-dinh-thi-cong-xay-nha-tron-goi/';
