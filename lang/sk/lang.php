fcf7fe17b22ac56f0c222a1018a772475a5eab2b
Wizzard <wizzardsk@gmail.com>
2026-06-03 20:25:52 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['reveal']                = 'odkryť';
+$lang['reveallong']            = 'odkryť skrytý obsah';
+$lang['hide']                  = 'skryť';
+$lang['hidelong']              = 'skryť obsah';
+$lang['fold_unfold_all_button'] = 'Zbaliť/rozbaliť všetko';
